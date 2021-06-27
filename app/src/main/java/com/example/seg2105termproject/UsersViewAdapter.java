0deaa4ad0e76b81611b1f44e25ef666b777caabb
SalRaad25 <sallyr_25@outlook.com>
2021-06-26 21:13:10 -0400

    Added my email

--- app/src/main/java/com/example/seg2105termproject/UsersViewAdapter.java
+++ app/src/main/java/com/example/seg2105termproject/UsersViewAdapter.java
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
  * app/src/main/java/com/example/seg2105termproject
  *
  * University of Ottawa - Faculty of Engineering - SEG2105 -Course Booking application for Android devices
- * @author      Sally R       <@uottawa.ca> 
+ * @author      Sally R       <sraad062@uottawa.ca>
  *              Jerry S       <jsoon029@uottawa.ca>
  *              Glen W        <@uottawa.ca>
  *              Youssef J     <yjall032@uottawa.ca>
--- app/src/main/java/com/example/seg2105termproject/Utils.java
+++ app/src/main/java/com/example/seg2105termproject/Utils.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
  * app/src/main/java/com/example/seg2105termproject
  *
  * University of Ottawa - Faculty of Engineering - SEG2105 -Course Booking application for Android devices
- * @author      Sally R       <@uottawa.ca> 
+ * @author      Sally R       <sraad062@uottawa.ca>
  *              Jerry S       <jsoon029@uottawa.ca>
  *              Glen W        <@uottawa.ca>
  *              Youssef J     <yjall032@uottawa.ca>
