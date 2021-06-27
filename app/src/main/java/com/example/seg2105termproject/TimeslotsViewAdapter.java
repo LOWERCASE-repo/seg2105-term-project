7b620561c033f3ed44851f96046e8c99703b2250
Jerry Soong <60661834+RoboticJ4000@users.noreply.github.com>
2021-06-27 16:45:16 -0400

    Added class JavaDoc to new classes.

--- app/src/main/java/com/example/seg2105termproject/TimeslotsViewAdapter.java
+++ app/src/main/java/com/example/seg2105termproject/TimeslotsViewAdapter.java
@@ -11,7 +11,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
-
+/**
+ * This file is part of Course Booking application for Android devices
+ *
+ * app/src/main/java/com/example/seg2105termproject
+ *
+ * University of Ottawa - Faculty of Engineering - SEG2105 -Course Booking application for Android devices
+ * @author      Sally R       <sraad062@uottawa.ca>
+ *              Jerry S       <jsoon029@uottawa.ca>
+ *              Glen W        <@uottawa.ca>
+ *              Youssef J     <yjall032@uottawa.ca>
+ *
+ */
 public class TimeslotsViewAdapter extends RecyclerView.Adapter<TimeslotsViewAdapter.TimeslotsHolder>{
 
     private final RecyclerView rView;
--- app/src/main/java/com/example/seg2105termproject/TimetableActivity.java
+++ app/src/main/java/com/example/seg2105termproject/TimetableActivity.java
@@ -14,6 +14,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
+/**
+ * This file is part of Course Booking application for Android devices
+ *
+ * app/src/main/java/com/example/seg2105termproject
+ *
+ * University of Ottawa - Faculty of Engineering - SEG2105 -Course Booking application for Android devices
+ * @author      Sally R       <sraad062@uottawa.ca>
+ *              Jerry S       <jsoon029@uottawa.ca>
+ *              Glen W        <@uottawa.ca>
+ *              Youssef J     <yjall032@uottawa.ca>
+ *
+ */
 public class TimetableActivity extends AppCompatActivity {
 
     public static final String TIMESLOT_POSITION = "com.example.seg2105termproject.TIMESLOT_POSITION";
